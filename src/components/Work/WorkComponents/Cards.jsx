4bc0e14cb714c5fa5e4fe465cd6dc3f7709147d3
Fede Rocha <federochacueli@gmail.com>
2024-08-02 03:36:34 -0300

    changes

--- src/components/Work/WorkComponents/Cards.jsx
+++ src/components/Work/WorkComponents/Cards.jsx
@@ -1,22 +1,58 @@
-import React from 'react'
+import React from 'react';
 
-import { Fade } from "react-awesome-reveal";
+// import { Fade } from "react-awesome-reveal";
+
+import WebIcon from '../../../assets/img/icons/web.svg';
+import LeftArrow from '../../../assets/img/icons/left-arrow.svg';
+import RightArrow from '../../../assets/img/icons/right-arrow.svg';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 
 export default function Cards({ styles, getWorkData, motion }) {
 	return (
-		<Fade duration={2000} delay={800}>
-			<div className={styles.work__cards}>
-				{getWorkData().map((img, index) => (
-					<div key={index}
-						className={styles.work__card}>
+		// <Fade duration={2000} delay={800}>
+		<Swiper
+			modules={[Navigation]}
+			navigation={{ // Configuración de los botones de navegación
+				nextEl: `.${styles.work__arrowNext}`,
+				prevEl: `.${styles.work__arrowPrev}`,
+			}}
+			loop={false} // Desactiva el loop si no deseas un bucle infinito
+			className={styles.work__cards}
+		>
+			{getWorkData().map((img, index) => (
+				<SwiperSlide key={index}>
+					<div className={styles.work__card}>
 						<img src={img.png} alt={img.name} />
 						<div className={styles.work__cardText}>
 							<h4>{img.name}</h4>
 							<p>{img.description}</p>
+							{
+								img.link != '' ?
+									<a href={img.link} target='_blank'>
+										<img src={WebIcon} alt="" />
+									</a>
+									:
+									<></>
+							}
 						</div>
 					</div>
-				))}
+				</SwiperSlide>
+			))}
+			{/* Botones de navegación */}
+			<div className={styles.work__arrowBtns}>
+				<div className={styles.work__arrowNext}>
+					<img src={RightArrow} alt="" />
+				</div>
+				<div className={styles.work__arrowPrev}>
+					<img src={LeftArrow} alt="" />
+				</div>
 			</div>
-		</Fade>
-	)
+		</Swiper>
+		// </Fade>
+	);
 }
--- src/components/Work/WorkData/workData.js
+++ src/components/Work/WorkData/workData.js
@@ -9,28 +9,88 @@ import { useLanguage } from '../../../context/LanguageContext';
 
 export default function getWorkData() {
 	const { language } = useLanguage();
- 
+
 	let workData = [];
- 
+
 	if (language === 'es') {
-	    workData = [
-		    { name: 'Nube Code', png: NubeCode, description: 'Para NubeCode, desarrollé una landing page de diseño limpio y funcional, centrada en ofrecer una experiencia clara y directa para los visitantes.' },
-		    { name: 'Productrivia', png: Productrivia, description: 'En el proyecto Productrivia, me enfoqué en la creación de la estructura y diseño del sitio web, así como en la implementación de funcionalidades adicionales para enriquecer la experiencia del usuario.' },
-		    { name: 'NubeMenu', png: NubeMenu, description: 'NubeMenu representó un desafío completo en el que asumí un rol integral. Desde el diseño hasta el desarrollo y la implementación de funcionalidades, me dediqué a crear una experiencia óptima para el usuario. Esto incluyó el diseño de la interfaz de usuario (UX/UI), el desarrollo del front-end y la integración de todas las funcionalidades con el backend, garantizando un producto final cohesivo y efectivo.'},
-		    { name: 'App Dolar', png: AppDolar, description: 'Esta es una app mobile sencilla de uso personal, la cual diseñe y desarrolle para poner en practica mis conocimientos en React Native.'},
-		    { name: 'Jose Maria Mineria', png: JoseMaria, description: 'Contribuí al diseño y desarrollo del sitio web para el proyecto José María Minería, desplegando habilidades de construcción e implementación de funcionalidades interactivas y optimización de la experiencia del usuario.' },
-		   { name: 'Ciicap', png: Ciicap, description: 'Me enfoqué en el esarrollo del layout del sitio web para Ciicap, creando la estructura fundamental que garantizó una navegación fluida y una presentación visualmente atractiva del contenido.' },
-	    ];
+		workData = [
+			{
+				name: 'Nube Code',
+				png: NubeCode,
+				description: 'Para NubeCode, desarrollé una landing page de diseño limpio y funcional, centrada en ofrecer una experiencia clara y directa para los visitantes.',
+				link: 'https://nubecode.ar/',
+			},
+			{
+				name: 'Productrivia',
+				png: Productrivia,
+				description: 'En el proyecto Productrivia, me enfoqué en la creación de la estructura y diseño del sitio web, así como en la implementación de funcionalidades adicionales para enriquecer la experiencia del usuario.',
+				link: '',
+			},
+			{
+				name: 'NubeMenu',
+				png: NubeMenu,
+				description: 'NubeMenu representó un desafío completo en el que asumí un rol integral. Desde el diseño hasta el desarrollo y la implementación de funcionalidades, me dediqué a crear una experiencia óptima para el usuario. Esto incluyó el diseño de la interfaz de usuario (UX/UI), el desarrollo del front-end y la integración de todas las funcionalidades con el backend, garantizando un producto final cohesivo y efectivo.',
+				link: '',
+			},
+			{
+				name: 'App Dolar',
+				png: AppDolar,
+				description: 'Esta es una app mobile sencilla de uso personal, la cual diseñe y desarrolle para poner en practica mis conocimientos en React Native.',
+				link: '',
+			},
+			{
+				name: 'Jose Maria Mineria',
+				png: JoseMaria,
+				description: 'Contribuí al diseño y desarrollo del sitio web para el proyecto José María Minería, desplegando habilidades de construcción e implementación de funcionalidades interactivas y optimización de la experiencia del usuario.',
+				link: '',
+			},
+			{
+				name: 'Ciicap',
+				png: Ciicap,
+				description: 'Me enfoqué en el esarrollo del layout del sitio web para Ciicap, creando la estructura fundamental que garantizó una navegación fluida y una presentación visualmente atractiva del contenido.',
+				link: 'https://instituto.nodoshub.com/',
+			},
+		];
 	} else {
-	    workData = [
-		    { name: 'Nube Code', png: NubeCode, description: 'For NubeCode, I developed a clean and functional landing page, focused on providing a clear and straightforward experience for visitors.' },
-		    { name: 'Productrivia', png: Productrivia, description: 'In the Productrivia project, I focused on creating the structure and design of the website, as well as implementing additional features to enhance the user experience.' },
-		    { name: 'NubeMenu', png: NubeMenu, description: 'NubeMenu represented a complete challenge where I took on an integral role. From design to development and implementation of features, I dedicated myself to creating an optimal user experience. This included designing the user interface (UI/UX), frontend development, and integrating all features with the backend, ensuring a cohesive and effective final product.'},
-		    { name: 'App Dolar', png: AppDolar, description: 'This is a simple mobile app for personal use, which I designed and developed to put my React Native skills into practice.'},
-		    { name: 'Jose Maria Mineria', png: JoseMaria, description: 'I contributed to the design and development of the website for the José María Mining project, deploying skills in building and implementing interactive features and optimizing the user experience.' },
-		   { name: 'Ciicap', png: Ciicap, description: 'I focused on developing the layout of the website for Ciicap, creating the fundamental structure that ensured smooth navigation and visually appealing presentation of content.' },
-	    ];
+		workData = [
+			{
+				name: 'Nube Code',
+				png: NubeCode,
+				description: 'For NubeCode, I developed a clean and functional landing page, focused on providing a clear and straightforward experience for visitors.',
+				link: 'https://nubecode.ar/',
+			},
+			{
+				name: 'Productrivia',
+				png: Productrivia,
+				description: 'In the Productrivia project, I focused on creating the structure and design of the website, as well as implementing additional features to enhance the user experience.',
+				link: '',
+			},
+			{
+				name: 'NubeMenu',
+				png: NubeMenu,
+				description: 'NubeMenu represented a complete challenge where I took on an integral role. From design to development and implementation of features, I dedicated myself to creating an optimal user experience. This included designing the user interface (UI/UX), frontend development, and integrating all features with the backend, ensuring a cohesive and effective final product.',
+				link: '',
+			},
+			{
+				name: 'App Dolar',
+				png: AppDolar,
+				description: 'This is a simple mobile app for personal use, which I designed and developed to put my React Native skills into practice.',
+				link: '',
+			},
+			{
+				name: 'Jose Maria Mineria',
+				png: JoseMaria,
+				description: 'I contributed to the design and development of the website for the José María Mining project, deploying skills in building and implementing interactive features and optimizing the user experience.',
+				link: '',
+			},
+			{
+				name: 'Ciicap',
+				png: Ciicap,
+				description: 'I focused on developing the layout of the website for Ciicap, creating the fundamental structure that ensured smooth navigation and visually appealing presentation of content.',
+				link: 'https://instituto.nodoshub.com/',
+			},
+		];
 	}
- 
+
 	return workData;
- }
+}
